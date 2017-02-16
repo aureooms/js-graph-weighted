1a527a6b4f26e4559910e1621f6132e9849bbf8d
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-02-16 11:35:39 +0100

    aureooms- > @aureooms/ : update js code

--- js/dist/graph-weighted.js
+++ js/dist/graph-weighted.js
@@ -13,10 +13,10 @@ if ( typeof exports === "object" ) {
 	definition( exports ) ;
 }
 else if ( typeof define === "function" && define.amd ) {
-	define( "aureooms-js-graph-weighted" , [ ] , function ( ) { return definition( { } ) ; } ) ;
+	define( "@aureooms/js-graph-weighted" , [ ] , function ( ) { return definition( { } ) ; } ) ;
 }
 else if ( typeof window === "object" && typeof window.document === "object" ) {
 	definition( window["graphweighted"] = { } ) ;
 }
-else console.error( "unable to detect type of module to define for aureooms-js-graph-weighted") ;
+else console.error( "unable to detect type of module to define for @aureooms/js-graph-weighted") ;
 } )( ) ;
